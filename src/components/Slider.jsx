5d4f6e3fafb1057724f236fb0041ee15164fd219
MdSakifHossain <sakif015sakif@gmail.com>
2025-11-04 02:32:29 +0600

    feat(Slider): implement shuffle logic to display a random subset of slides

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -7,6 +7,15 @@ import "swiper/css/pagination";
 import { Autoplay, Pagination } from "swiper/modules";
 
 const Slider = ({ data }) => {
+  // Fisher–Yates (Knuth) shuffle algorithm
+  const shuffleArray = (array) => {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  };
+
   return (
     <Swiper
       spaceBetween={30}
@@ -22,28 +31,30 @@ const Slider = ({ data }) => {
       modules={[Autoplay, Pagination]}
       className="w-full"
     >
-      {data.slice(0, 11).map((data) => (
-        <SwiperSlide>
-          <Link
-            to={`/games/${data.id}`}
-            className="font-outfit lg:grid lg:grid-cols-2 select-none"
-          >
-            <div className="hidden px-8 lg:flex items-start justify-center flex-col gap-4">
-              <h2 className="text-5xl font-bold border-l-8 border-violet-500 ps-4 ">
-                {data.title}
-              </h2>
-              <p className="text-lg font-medium text-gray-300">
-                {data.short_description}
-              </p>
-            </div>
-            <img
-              className="w-full rounded-3xl"
-              src={data.thumbnail}
-              alt={data.title}
-            />
-          </Link>
-        </SwiperSlide>
-      ))}
+      {shuffleArray(data)
+        .slice(0, 11)
+        .map((data) => (
+          <SwiperSlide>
+            <Link
+              to={`/games/${data.id}`}
+              className="font-outfit lg:grid lg:grid-cols-2 select-none"
+            >
+              <div className="hidden px-8 lg:flex items-start justify-center flex-col gap-4">
+                <h2 className="text-5xl font-bold border-l-8 border-violet-500 ps-4 ">
+                  {data.title}
+                </h2>
+                <p className="text-lg font-medium text-gray-300">
+                  {data.short_description}
+                </p>
+              </div>
+              <img
+                className="w-full rounded-3xl"
+                src={data.thumbnail}
+                alt={data.title}
+              />
+            </Link>
+          </SwiperSlide>
+        ))}
     </Swiper>
   );
 };
